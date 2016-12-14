3fe3b26f11729b1eff46344142cd2ff72d8e9131
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-12-14 16:23:54 +0530

    Add .nvmrc

--- src/components/PinDiagram.vue
+++ src/components/PinDiagram.vue
@@ -1,8 +1,303 @@
 <template lang="pug">
+#pin-diagram
+  <svg id="pinDiagramSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 744.09448819 1052.3622047">
+    <g font-size="40" font-family="sans-serif" letter-spacing="0" word-spacing="0">
+      <text x="585.40179" y="29.505333" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="585.40179" y="29.505333">Vcc</tspan>
+      </text>
+      <text x="581.78851" y="1035.2194" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="1035.2194">P2.0</tspan>
+      </text>
+      <text x="581.78851" y="982.61908" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="982.61908">P2.1</tspan>
+      </text>
+      <text x="581.78851" y="930.01874" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="930.01874">P2.2</tspan>
+      </text>
+      <text x="581.78851" y="876.85205" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="876.85205">P2.3</tspan>
+      </text>
+      <text x="581.78851" y="824.25177" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="824.25177">P2.4</tspan>
+      </text>
+      <text x="581.78851" y="771.08502" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="771.08502">P2.5</tspan>
+      </text>
+      <text x="581.78851" y="717.91833" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="717.91833">P2.6</tspan>
+      </text>
+      <text x="581.78851" y="665.31805" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="665.31805">P2.7</tspan>
+      </text>
+      <text x="581.78851" y="612.15137" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="612.15137">PSEN</tspan>
+      </text>
+      <text x="585.40179" y="559.55109" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="585.40179" y="559.55109">ALE</tspan>
+      </text>
+      <text x="581.78851" y="507.47818" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="507.47818">EA</tspan>
+      </text>
+      <text x="581.78851" y="82.67202" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="82.67202">P0.0</tspan>
+      </text>
+      <text x="581.78851" y="135.8387" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="135.8387">P0.1</tspan>
+      </text>
+      <text x="581.78851" y="189.00539" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="189.00539">P0.2</tspan>
+      </text>
+      <text x="581.78851" y="242.17207" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="242.17207">P0.3</tspan>
+      </text>
+      <text x="581.78851" y="295.33878" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="295.33878">P0.4</tspan>
+      </text>
+      <text x="581.78851" y="348.50546" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="348.50546">P0.5</tspan>
+      </text>
+      <text x="581.78851" y="401.67215" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="401.67215">P0.6</tspan>
+      </text>
+      <text x="581.78851" y="454.83884" style="line-height:125%" transform="translate(20.333 3.998)">
+        <tspan x="581.78851" y="454.83884">P0.7</tspan>
+      </text>
+    </g>
+    <g font-size="40" font-family="sans-serif" letter-spacing="0" word-spacing="0">
+      <text x="581.40179" y="29.505333" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.40179" y="29.505333">P1.0</tspan>
+      </text>
+      <text x="581.78851" y="1035.2194" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="1035.2194">GND</tspan>
+      </text>
+      <text x="581.78851" y="982.61908" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="982.61908">XTAL1</tspan>
+      </text>
+      <text x="581.78851" y="930.01874" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="930.01874">XTAL2</tspan>
+      </text>
+      <text x="581.78851" y="876.85205" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="876.85205">P3.7</tspan>
+      </text>
+      <text x="581.78851" y="824.25177" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="824.25177">P3.6</tspan>
+      </text>
+      <text x="581.78851" y="771.08502" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="771.08502">P3.5</tspan>
+      </text>
+      <text x="581.78851" y="717.91833" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="717.91833">P3.4</tspan>
+      </text>
+      <text x="581.78851" y="665.31805" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="665.31805">P3.3</tspan>
+      </text>
+      <text x="581.78851" y="612.15137" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="612.15137">P3.2</tspan>
+      </text>
+      <text x="585.40179" y="559.55109" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="585.40179" y="559.55109">P3.1</tspan>
+      </text>
+      <text x="581.78851" y="507.47818" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="507.47818">P3.0</tspan>
+      </text>
+      <text x="581.78851" y="82.67202" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="82.67202">P1.1</tspan>
+      </text>
+      <text x="581.78851" y="135.8387" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="135.8387">P1.2</tspan>
+      </text>
+      <text x="581.78851" y="189.00539" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="189.00539">P1.3</tspan>
+      </text>
+      <text x="581.78851" y="242.17207" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="242.17207">P1.4</tspan>
+      </text>
+      <text x="581.78851" y="295.33878" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="295.33878">P1.5</tspan>
+      </text>
+      <text x="581.78851" y="348.50546" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="348.50546">P1.6</tspan>
+      </text>
+      <text x="581.78851" y="401.67215" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="401.67215">P1.7</tspan>
+      </text>
+      <text x="581.78851" y="454.83884" style="line-height:125%" transform="translate(-563.233 3.998)">
+        <tspan x="581.78851" y="454.83884">RST</tspan>
+      </text>
+    </g>
+    <rect width="398.99921" height="1040.142" x="-571.54681" y="7.2529106" fill="none" stroke="#000" stroke-width="1.00078225" transform="matrix(-1 0 0 1 0 -1.07)" ry="5.1715283" rx="4.3269048"/>
+    <g fill="none" stroke="#000">
+      <path d="M572.04721 19.252831h28.57142"/>
+      <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      <g transform="translate(0 54)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 106)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 160)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 212)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 264)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 318)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 372)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 426)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 478)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 528)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 582)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 636)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 688)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 742)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 796)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 848)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 900)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 952)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="translate(0 1006)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+    </g>
+    <g fill="none" stroke="#000">
+      <g transform="matrix(-1 0 0 1 744.094 0)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 54)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 106)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 160)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 212)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 264)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 318)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 372)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 426)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 478)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 528)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 582)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 636)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 688)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 742)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 796)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 848)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 900)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 952)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+      <g transform="matrix(-1 0 0 1 744.094 1006)">
+        <path d="M572.04721 19.252831h28.57142"/>
+        <circle cx="594.95624" cy="19.252831" r="5.1623631"/>
+      </g>
+    </g>
+  </svg>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
+  computed: mapState([
+    'memory',
+  ]),
 };
 </script>
 
