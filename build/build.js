496c3b80f07f2b90e20a6766fd47cbdf8712010f
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-12-24 21:27:00 +0530

    Trial deploy to Github Pages (2)

--- build/build.js
+++ build/build.js
@@ -3,7 +3,6 @@ require('./check-versions')()
 require('shelljs/global')
 env.NODE_ENV = 'production'
 
-var path = require('path')
 var config = require('../config')
 var ora = require('ora')
 var webpack = require('webpack')
@@ -18,7 +17,7 @@ console.log(
 var spinner = ora('building for production...')
 spinner.start()
 
-var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+var assetsPath = config.build.assetsRoot
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
