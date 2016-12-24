03ddd20282f472767859ebb3c669cb3394f506cf
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-12-24 23:45:47 +0530

    Trial deploy to Github Pages (Last try)
    diff --git a/config/index.js b/config/index.js
    index 59374f0..bddc509 100644
    --- a/config/index.js
    +++ b/config/index.js
    @@ -7,7 +7,7 @@ module.exports = {
         port: 8080,
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
    -    assetsSubDirectory: '',
    +    assetsSubDirectory: '8051.js',
         assetsPublicPath: '/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     port: 8080,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '',
+    assetsSubDirectory: '8051.js',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
