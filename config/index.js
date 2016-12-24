081da6b3642db702c08701c40136aaf237baeff2
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2016-12-24 23:59:43 +0530

    Last try to deploy to Github Pages
    Hope this works.

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     port: 8080,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '8051.js',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
