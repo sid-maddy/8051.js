cd7bcccd023d896215e40c7b0894cccb84e3e8d9
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2017-07-12 00:59:39 +0530

    Fix default route issue and update packages

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,6 @@
 
 <script>
 import 'semantic-ui-css/semantic.min.css';
-import { replace } from 'lodash';
 
 import IntroModal from './components/IntroModal';
 import PinDiagram from './components/PinDiagram';
